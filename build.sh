92f078816ba63f4b5e93c8e6472d861f70f4b3f1
Jakob Kallin <jakobkallin@gmail.com>
2015-07-26 14:55:03 +0200

    Build script

---
+++ build.sh
@@ -0,0 +1,44 @@
+shopt -s globstar
+
+function compile {
+    local SOURCE=${1}
+    local TARGET=${2}
+    
+    # Copy files
+    rsync \
+        --recursive \
+        --links \
+        --delete \
+        --delete-excluded \
+        --exclude=.git \
+        --exclude=.sass-cache \
+        $SOURCE $TARGET
+    echo "Copy: $(realpath $SOURCE) => $(realpath $TARGET)"
+    
+    # Compile ES6 to JS
+    for ES6 in $TARGET/**/*.es6
+    do
+        JS="${ES6%.*}.js"
+        echo "Compile: ${ES6#$TARGET} => ${JS#$TARGET}"
+        babel "$ES6" > "$JS"
+    done
+    
+    # Compile SCSS to CSS
+    sass --update $TARGET/css:$TARGET/css
+}
+
+if [ "$#" -ne 2 ]
+then
+    echo "Usage: build.sh <source> <target>"
+    exit
+fi
+
+SOURCE="$1"
+TARGET="$2"
+
+compile $SOURCE $TARGET
+
+while inotifywait --exclude "\.git" --recursive --event=create,modify,delete,move $SOURCE
+do
+    compile $SOURCE $TARGET
+done
